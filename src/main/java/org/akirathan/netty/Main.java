cf4d7df093eb9f294f40a9e75d6c0782e121e966
Pavel Marek <pavel.marek@enso.org>
2025-07-07 13:15:58 +0200

    Initial commit

---
+++ src/main/java/org/akirathan/netty/Main.java
@@ -0,0 +1,63 @@
+package org.akirathan.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.DefaultHttpHeadersFactory;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.ssl.SslProvider;
+import io.netty.internal.tcnative.Library;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  private volatile static Class<?> libClass;
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    LOGGER.info("Starting netty playground...");
+    AzureConnector.listBlob();
+  }
+
+  private static void ensureClassesInNI() {
+    libClass = Library.class;
+  }
+
+  private static void loadNativeLibEntrypoint() {
+    var libPath = System.getProperty("java.library.path");
+    System.out.println("libPath = " + libPath);
+    ensureClassesInNI();
+    loadNativeLib();
+  }
+
+  private static void loadNativeLib() {
+    var libName = "netty_tcnative_linux_x86_64";
+    try {
+      System.loadLibrary(libName);
+    } catch (Throwable t) {
+      System.err.println("Failed to load native library: " + t.getMessage());
+      t.printStackTrace();
+      throw new RuntimeException("Failed to load native library", t);
+    }
+    System.out.println("Successfully loaded native library: " + libName);
+  }
+
+  /**
+   * {@code isAlpnSupported} is called from {@code AzureStorage.listBlob}.
+   * And it fails in Enso.
+   */
+  private static void tryLoadSslProvider() {
+    var isAlpnSupported = SslProvider.isAlpnSupported(SslProvider.OPENSSL);
+    System.out.println("isAlpnSupported on OpenSSL = " + isAlpnSupported);
+  }
+
+  /**
+   * Works fine in both JVM and NI.
+   */
+  private static void createHttpHeader() {
+    var headerFac = DefaultHttpHeadersFactory.headersFactory();
+    var header = headerFac.newHeaders();
+    header.add(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+    header.add(HttpHeaderNames.CONTENT_LENGTH, "0");
+    System.out.println("header.toString() = " + header);
+  }
+}
